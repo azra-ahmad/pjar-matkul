71e2ef7955f5282f0dd35156f68115c98e3cd742
azra-ahmad <ajrahari@gmail.com>
2026-04-21 20:57:03 +0700

    udp

---
+++ udp_client.py
@@ -0,0 +1,13 @@
+import socket
+
+UDP_IP = "192.168.43.184" 
+UDP_PORT = 8501
+MESSAGE = b"Halo Azra, ini paket UDP dari Windows!"
+
+print(f"Nembak paket ke {UDP_IP}:{UDP_PORT}...")
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+print("Pesan terkirim!")
+
